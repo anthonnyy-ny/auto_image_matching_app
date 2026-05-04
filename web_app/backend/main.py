c40fde2de5c2c774bb28be10b3a08f1cd94ba131
Chuyanglife <yaohong96@gmail.com>
2026-05-04 20:30:53 +0800

    ok

--- web_app/backend/main.py
+++ web_app/backend/main.py
@@ -36,7 +36,11 @@ except ImportError:
 app = FastAPI(title="Auto Image Matching Web API", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://www.127.0.0.1.sslip.io:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
